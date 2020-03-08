c1970fd11d2eaaa512b7d52bac3f3c1be8dd8e62
nyitraijoco <nyitrai.joco@gmail.com>
2020-03-08 07:40:54 +0100

    2 files modified, 1 added

---
+++ app/classes/attribute.ts
@@ -0,0 +1,8 @@
+export class Attribute{
+  public main: string;
+  public active: string;
+  public activepoint: number;
+  public passive: string;
+  public passivepoint: number;
+
+}
--- pages/attributes/attributes.ts
+++ pages/attributes/attributes.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HeaderPage } from '../header/header';
+import { Attribute } from '../../classes/attribute';
 
 @Component({
   selector: 'page-attributes',
   templateUrl: 'attributes.html'
 })
 export class AttributesPage {
-  public physique: string;
+  public physique: Attribute = new Attribute();
   public strength: string;
   public constitution: string
   constructor(public navCtrl: NavController) {
-
   }
 
 }
